bbcdbd1f2a8d62e4bf81893cef818ae8283a8eb1
Muhammad6196 <65651659+Muhammad6196@users.noreply.github.com>
2022-09-28 21:46:17 +0500

    Update alt1.lua

--- alt1.lua
+++ alt1.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Tests/main/alt.lua"))
-queue_on_teleport(loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/XXXtr1vs/main/damn.lua")))
+queue_on_teleport(loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Tests/main/alt1.lua")))
