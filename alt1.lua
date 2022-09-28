70688add64748ea6f8243ad2162a63fa7638d7ee
Muhammad6196 <65651659+Muhammad6196@users.noreply.github.com>
2022-09-28 21:43:57 +0500

    Update alt1.lua

--- alt1.lua
+++ alt1.lua
@@ -1,1 +1,2 @@
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/XXXtr1vs/main/althi.lua"))
+queue_on_teleport(loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/XXXtr1vs/main/damn.lua")))
