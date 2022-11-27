2c154d079d6614bd3bf11ec686d56e93fbc43829
ChickenOnFloor <115373612+ChickenOnFloor@users.noreply.github.com>
2022-11-28 02:31:45 +0500

    Update gaysum.lua

--- gaysum.lua
+++ gaysum.lua
@@ -3,3 +3,11 @@ local updateinv = getsenv(game:GetService("Players").LocalPlayer.PlayerScripts.S
         return true
     end)
     updateinv.Update()
+
+local cmod = getsenv(game:GetService("Players").LocalPlayer.PlayerScripts.Scripts.Game.Lootbags)
+    hookfunction(cmod.ScanForCollection, function(...)
+        for i,v in pairs(game:GetService("Workspace")["__THINGS"].Lootbags:GetChildren()) do
+            v:SetAttribute("Collected")
+            cmod.Collect(v)
+        end
+    end)
