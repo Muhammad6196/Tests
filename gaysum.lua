387016fd630a27aa546e17c6f821e1c03a25cb88
ChickenOnFloor <115373612+ChickenOnFloor@users.noreply.github.com>
2022-11-28 02:30:55 +0500

    Update gaysum.lua

--- gaysum.lua
+++ gaysum.lua
@@ -1,10 +1,5 @@
-local netmod = getsenv(game:GetService("ReplicatedStorage").Framework.Client["2 | Network"])
-    for i,v in pairs(getgc(true)) do
-        if type(v) == "function" and getfenv(v).script == game:GetService("ReplicatedStorage").Framework.Client["2 | Network"] then
-            if table.find(getconstants(v), "settings") then
-                local Hook = hookfunction(v, function(...)
-                    return true
-                end)
-            end
-        end
-    end
+local updateinv = getsenv(game:GetService("Players").LocalPlayer.PlayerScripts.Scripts.GUIs.Inventory)
+    hookfunction(updateinv.isVisible, function()
+        return true
+    end)
+    updateinv.Update()
