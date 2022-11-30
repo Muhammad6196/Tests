2e27be65abd03b4fc54925625a0e58a5c141fe9d
ChickenOnFloor <115373612+ChickenOnFloor@users.noreply.github.com>
2022-11-30 19:30:51 +0500

    Update gaysum.lua

--- gaysum.lua
+++ gaysum.lua
@@ -1,3 +1,23 @@
+local network = require(game:GetService("ReplicatedStorage").Framework.Client["2 | Network"])
+
+    e = getupvalues(network.Invoke)[2]
+    f = getupvalues(network.Invoke)[3]
+    g = getupvalues(network.Fire)[2]
+    h = getupvalues(network.Fire)[3]
+    hookfunction(network.Invoke, function(a, ...)
+        local c = e(a);
+        if c then
+            return c:InvokeServer(...);
+        end
+        return f(a):Invoke(...);
+    end)
+    hookfunction(network.Fire, function(a, ...)
+        local c = g(a);
+        if c then
+            return c:FireServer(...);
+        end
+        return h(a):Fire(...);
+    end)
 local updateinv = getsenv(game:GetService("Players").LocalPlayer.PlayerScripts.Scripts.GUIs.Inventory)
     hookfunction(updateinv.isVisible, function()
         return true
