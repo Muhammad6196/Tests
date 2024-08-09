d5033240e8ba4e10d02cf916655c595fab135ba3
Muhammad6196 <65651659+Muhammad6196@users.noreply.github.com>
2024-08-09 13:08:16 +0500

    Update Main.lua

--- wd_Arise/Main.lua
+++ wd_Arise/Main.lua
@@ -1,3 +1,3 @@
-if game.PlaceId == 17503543197 or game.PlaceId == 16498369169 or game.PlaceId == 8737899170 then
+if game.PlaceId == 17503543197 or game.PlaceId == 16498369169 or game.PlaceId == 8737899170 or game.PlaceId == 15588442388 or game.PlaceId == 15502339080 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Tests/main/wd_Arise/scripts/ps99.lua"))()
 end
