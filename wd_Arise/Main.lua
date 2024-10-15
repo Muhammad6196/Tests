02adab28b79736422c334d42ff4f35bcd7b2aad3
Muhammad6196 <65651659+Muhammad6196@users.noreply.github.com>
2024-10-16 01:55:56 +0500

    Update Main.lua

--- wd_Arise/Main.lua
+++ wd_Arise/Main.lua
@@ -1,3 +1,5 @@
 if game.PlaceId == 17503543197 or game.PlaceId == 16498369169 or game.PlaceId == 8737899170 or game.PlaceId == 15588442388 or game.PlaceId == 15502339080 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Tests/main/wd_Arise/scripts/ps99.lua"))()
+else if game.PlaceId == 18901165922 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Tests/main/wd_Arise/scripts/pets_go.lua"))()
 end
